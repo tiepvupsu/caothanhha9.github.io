f953218e3d5a19cc55bd63be7764c06f66ba286d
hact <caothanhha9@gmail.com>
2018-11-04 22:14:07 +0700

    update page

---
+++ precache-manifest.77f558bf784f473fbf0f53c318740b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3dc5fea2966be5ca98a",
+    "url": "/static/css/main.e1c9d910.chunk.css"
+  },
+  {
+    "revision": "b3dc5fea2966be5ca98a",
+    "url": "/static/js/main.b3dc5fea.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8b4746db005c0cc1b286255af5b7176",
+    "url": "/static/media/bg-countdown.b8b4746d.jpg"
+  },
+  {
+    "revision": "7cb5ebe17856d6084fc1f83a125d0bb0",
+    "url": "/index.html"
+  }
+];
